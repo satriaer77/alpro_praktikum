c49faf43e113f02ba76c23784ab157014c03006d
satriaer77 <ppicbimasatria@gmail.com>
2021-09-09 08:57:11 +0700

    Merapikan direktori

---
+++ Pertemuan_2/luaslingkaransmp.py
@@ -0,0 +1,15 @@
+"""
+Program Luas Lingkaran
+"""
+
+pi = 3.14
+ 
+r = float(input("==>  Masukkan jari-jari : "))
+
+luaslingkaran = pi*r**2
+
+print("""
+        =------------------------------------------------------------=
+        Jadi luas lingkaran dengan r=""",r,""" adalah """,luaslingkaran,"""
+        =------------------------------------------------------------=
+        """)
---
+++ Pertemuan_2/tempconvsmp.py
@@ -0,0 +1,13 @@
+"""
+Program Konversi Suhu
+"""
+
+celsius = float(input(" ==>  Masukkan Suhu celsius : ")) 
+ 
+konversiFarenheit = (9/6 * celsius)+32
+
+print("""
+        =------------------------------------------------------------=
+        Jadi Suhu """,celsius,""" derajat C ---> F adalah """,konversiFarenheit,"""derajat F
+        =------------------------------------------------------------=
+        """)
